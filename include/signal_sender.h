bdbf0df891a10ea57d70c7c3833d7a45a600b9c5
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-03-22 18:44:58 +0100

    up

--- include/signal_sender.h
+++ include/signal_sender.h
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   signal_sender.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 13:33:54 by swaegene          #+#    #+#             */
-/*   Updated: 2022/03/22 15:38:20 by swaegene         ###   ########.fr       */
+/*   Created: 2022/03/22 15:48:40 by swaegene          #+#    #+#             */
+/*   Updated: 2022/03/22 18:34:58 by swaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ft_printf.h>
-#include <unistd.h>
+#ifndef SIGNAL_SENDER_H
+# define SIGNAL_SENDER_H
 
-int	start_server(void)
-{
-	while (1)
-	{
-		usleep(30000);
-		ft_printf("run");
-	}
-	return (0);
-}
+int		send_message_to_pid(char *p, char *msg);
+void	ctob(char c, int b_msg[8]);
 
-int	main(void)
-{
-	pid_t	pid;
-
-	pid = getpid();
-	ft_printf("%d\n", pid);
-	return (start_server());
-}
+#endif
--- src/client.c
+++ src/client.c
@@ -6,15 +6,16 @@
 /*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 13:33:25 by swaegene          #+#    #+#             */
-/*   Updated: 2022/03/22 14:09:54 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/03/22 18:33:29 by swaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_printf.h>
+#include <signal_sender.h>
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	ft_printf("client");
+	if (argc == 3)
+		return (send_message_to_pid(argv[1], argv[2]));
+	return (0);
 }
